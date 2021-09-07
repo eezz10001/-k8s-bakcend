eec152d1b13cc58bf050ae800f2468894faf1a36
GrootYuan <8612707+grootyuan@user.noreply.gitee.com>
2021-09-07 09:30:20 +0800

    x

--- src/services/PodService.go
+++ src/services/PodService.go
@@ -1,13 +1,31 @@
 package services
 
+import "k8s-bakcend/src/models"
+
 //@Service
 type PodService struct {
 	PodMap *PodMapStruct `inject:"-"`
+	Common *CommonService `inject:"-"`
 }
 
 func NewPodService() *PodService {
 	return &PodService{}
 }
 func(this *PodService) ListByNs(ns string ) interface{}{
-	return this.PodMap.ListByNs(ns)
+	podList:=this.PodMap.ListByNs(ns)
+	ret:=make([]*models.Pod,0)
+	for _,pod:=range podList{
+		ret=append(ret,&models.Pod{
+			Name:pod.Name,
+			NameSpace:pod.Namespace,
+			Images:this.Common.GetImagesByPod(pod.Spec.Containers),
+			NodeName:pod.Spec.NodeName,
+			Phase:string(pod.Status.Phase),// 阶段
+			//IsReady:GetPodIsReady(*pod), //是否就绪
+			IP:[]string{pod.Status.PodIP,pod.Status.HostIP},
+			//Message:core.EventMap.GetMessage(pod.Namespace,"Pod",pod.Name),
+			CreateTime:pod.CreationTimestamp.Format("2006-01-02 15:04:05"),
+		})
+	}
+	return ret
 }
